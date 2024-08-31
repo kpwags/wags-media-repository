dc93057bd937ba77513645393ac2b5b999d8b19a
Keith Wagner <github@kpwags.com>
2024-08-30 20:19:07 -0400

    Added Podcast Filtering
    # Added Podcast Filtering
    
    ## Work Done
    
    - Added loading state to podcast grid
    - Added filters for podcasts (name & category)
    
    ## Associated Issue
    
    - Closes #18

--- node/wags-media-repository-web/src/pages/podcasts/PodcastsHome.tsx
+++ node/wags-media-repository-web/src/pages/podcasts/PodcastsHome.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
 import {
     Button,
+    Form,
+    Input,
     Row,
+    Select,
     Space,
+    Spin,
     Table,
     TableProps,
     Tag,
@@ -11,42 +15,105 @@ import {
 
 import { Api } from '@lib/api';
 
-import { Podcast } from '@models/Podcast';
+import { Podcast, PodcastCategory } from '@models/Podcast';
 import Confirmation from '@components/base/Confirmation';
 
-const { Title, Link } = Typography;
+const { Text, Title, Link } = Typography;
 
 const PodcastsHome = (): JSX.Element => {
+    const [allPodcasts, setAllPodcasts] = useState<Podcast[]>([]);
     const [podcasts, setPodcasts] = useState<Podcast[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [podcastCategories, setPodcastCategories] = useState<PodcastCategory[]>([]);
+    const [processingMessage, setProcessingMessage] = useState<string>('Loading...');
+    const [activeFilters, setActiveFilters] = useState<{ name: string, categoryId: number }>({ name: '', categoryId: 0 });
+
+    const fetchPodcasts = async (): Promise<[Podcast[] | null, string | null]> => await await Api.Get<Podcast[]>('podcasts');
+    const fetchPodcastCategories = async (): Promise<[PodcastCategory[] | null, string | null]> => {
+        return await await Api.Get<PodcastCategory[]>('podcasts/categories');
+    }
 
     const loadPodcasts = async () => {
-        const [data, error] = await Api.Get<Podcast[]>('podcasts');
+        const [data, error] = await fetchPodcasts();
 
         if (error) {
-            setLoading(false);
+            setProcessingMessage('');
             return;
         }
 
         setPodcasts(data ?? []);
-        setLoading(false);
+        setAllPodcasts(data ?? []);
+        setProcessingMessage('');
+    };
+
+    const loadPageData = async () => {
+        const [
+            [podcastData, podcastError],
+            [podcastCategoryData, podcastCatgeoryError]
+        ] = await Promise.all([
+            fetchPodcasts(),
+            fetchPodcastCategories(),
+        ]);
+
+        if (podcastError || podcastCatgeoryError) {
+            setProcessingMessage('');
+            return;
+        }
+
+        setPodcasts(podcastData ?? []);
+        setAllPodcasts(podcastData ?? []);
+        setPodcastCategories(podcastCategoryData ?? []);
+
+        setProcessingMessage('');
     };
 
     const deletePodcast = async (id: number) => {
-        setLoading(true);
+        setProcessingMessage('Deleting podcast...');
         const [, error] = await Api.Delete(`podcasts/${id}`);
 
         if (error) {
-            setLoading(false);
+            setProcessingMessage('');
             return;
         }
 
         await loadPodcasts();
     }
 
+    const filterPodcastsByName = (filterText: string) => {
+        setActiveFilters({
+            ...activeFilters,
+            name: filterText.trim(),
+        });
+    };
+
+    const filterPodcastsByCategory = (categoryId: number) => {
+        setActiveFilters({
+            ...activeFilters,
+            categoryId,
+        });
+    };
+
     useEffect(() => {
-        loadPodcasts();
+        loadPageData();
     }, []);
+
+    useEffect(() => {
+        if (activeFilters.categoryId === 0 && activeFilters.name.trim() === '') {
+            setPodcasts(allPodcasts);
+            return;
+        }
+
+        if (activeFilters.categoryId === 0 && activeFilters.name.trim() !== '') {
+            setPodcasts(allPodcasts.filter((p) => p.name.toLocaleLowerCase().includes(activeFilters.name.toLocaleLowerCase())));
+            return;
+        }
+
+        if (activeFilters.categoryId > 0 && activeFilters.name.trim() === '') {
+            setPodcasts(allPodcasts.filter((p) => p.podcastCategoryId === activeFilters.categoryId));
+            return;
+        }
+
+        setPodcasts(allPodcasts.filter((p) => p.podcastCategoryId === activeFilters.categoryId && p.name.toLocaleLowerCase().includes(activeFilters.name.toLocaleLowerCase())));
+    }, [allPodcasts, activeFilters]);
 
     const tableColumns: TableProps<Podcast>['columns'] = [
         {
@@ -95,30 +162,52 @@ const PodcastsHome = (): JSX.Element => {
     ]
 
     return (
-        <Space direction="vertical" size={24} className="full-width">
-            <Title level={1}>Podcasts</Title>
-            <Row justify="end">
-                <Space direction="horizontal" size={8}>
-                    <Button href="/podcasts/categories">
-                        Categories
-                    </Button>
-                    <Button
-                        type="primary"
-                        htmlType="button"
-                        onClick={() => { }}
-                    >
-                        Add New Podcast
-                    </Button>
-                </Space>
-            </Row>
-            <Table
-                columns={tableColumns}
-                dataSource={podcasts}
-                rowKey="podcastId"
-                pagination={false}
-                loading={loading}
-            />
-        </Space>
+        <Spin spinning={processingMessage !== ''} tip={processingMessage}>
+            <Space direction="vertical" size={24} className="full-width">
+                <Title level={1}>Podcasts</Title>
+                <Row justify="space-between" align="bottom" className="filter-actions">
+                    <Space direction="vertical" size={8} className="table-filter">
+                        <Text strong>Filters</Text>
+                        <Form.Item label="Name">
+                            <Input
+                                placeholder="Enter Podcast Name"
+                                onChange={(e) => {
+                                    filterPodcastsByName(e.target.value);
+                                }}
+                            />
+                        </Form.Item>
+                        <Form.Item label="Category">
+                            <Select
+                                style={{ width: 200 }}
+                                placeholder="Select Category"
+                                onChange={(e) => {
+                                    filterPodcastsByCategory(e);
+                                }}
+                                options={podcastCategories.map((pc) => ({ key: pc.podcastCategoryId, label: pc.name, value: pc.podcastCategoryId }))} />
+                        </Form.Item>
+                    </Space>
+                    <Space direction="horizontal" size={8}>
+                        <Button href="/podcasts/categories">
+                            Categories
+                        </Button>
+                        <Button
+                            type="primary"
+                            htmlType="button"
+                            onClick={() => { }}
+                        >
+                            Add New Podcast
+                        </Button>
+                    </Space>
+                </Row>
+                <Table
+                    columns={tableColumns}
+                    dataSource={podcasts}
+                    rowKey="podcastId"
+                    pagination={false}
+                    loading={processingMessage !== ''}
+                />
+            </Space>
+        </Spin>
     );
 }
 
