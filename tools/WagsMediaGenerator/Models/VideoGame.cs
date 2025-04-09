20e6ebb15ea4fad9fa57a077bcccb1667c80ea56
Keith Wagner <github@kpwags.com>
2025-04-09 19:47:13 -0400

    Fixed Completion Status

--- tools/WagsMediaGenerator/Models/VideoGame.cs
+++ tools/WagsMediaGenerator/Models/VideoGame.cs
@@ -3,28 +3,28 @@ namespace WagsMediaGenerator.Models;
 public class VideoGame
 {
     public int VideoGameId { get; set; }
-    
+
     public string Title { get; set; } = string.Empty;
-    
+
     public string Link { get; set; } = string.Empty;
-    
+
     public DateTime? DateStarted { get; set; }
-    
+
     public DateTime? DateCompleted { get; set; }
-    
+
     public int Rating { get; set; }
 
     public string Thoughts { get; set; } = string.Empty;
-    
+
     public string CoverImageUrl { get; set; } = string.Empty;
-    
+
     public int? SortOrder { get; set; }
 
     public Tag Status { get; set; } = new();
 
-    public Tag Completion = new();
+    public Tag Completion { get; set; } = new();
 
     public List<Tag> Genres { get; set; } = [];
-    
+
     public List<Tag> Systems { get; set; } = [];
 }
